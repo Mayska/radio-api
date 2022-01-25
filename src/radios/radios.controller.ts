d9919960131e05f802fa2105f2a83fc6a7b5989a
Mayska <>
2022-01-25 17:04:25 +0100

    ajout validateur

--- src/radios/radios.controller.ts
+++ src/radios/radios.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
 import { RadiosService } from './radios.service';
 import { Radios as RadiosModel } from '@prisma/client';
 
@@ -9,13 +9,18 @@ export class RadiosController {
         private readonly radiosService: RadiosService
     ) { }
 
+    @Post()
+    async createRadio(@Body() postData: { name: string, url_flux: string, url_img: string }): Promise<RadiosModel> {
+        return this.radiosService.createRadio(postData);
+    }
+
     @Get()
-    async getAllRadios(): Promise<RadiosModel[]> {
+    async findAllRadios(): Promise<RadiosModel[]> {
         return this.radiosService.findAll();
     }
 
     @Get('/order/:by')
-    async getAllRadiosOrderBy(@Param('by') by: string): Promise<RadiosModel[] | string> {
+    async findAllOrderBy(@Param('by') by: string): Promise<RadiosModel[] | string> {
         console.log(by);
         if (by === 'desc' || by === 'asc') {
             return this.radiosService.findAllOrderBy(by);
--- src/radios/radios.service.ts
+++ src/radios/radios.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import {
-    Radios,
-    Prisma,
+    Radios, Prisma
 } from '@prisma/client';
+
+
+const createRadioAndPost = (
+    name: string,
+    url_flux: string,
+    url_img?: string
+) => {
+    return Prisma.validator<Prisma.RadiosCreateInput>()({
+        name,
+        url_flux,
+        url_img,
+    })
+}
 
 @Injectable()
 export class RadiosService {
 
     constructor(private prisma: PrismaService) { }
+
+
+    async createRadio(data: Prisma.RadiosCreateInput): Promise<Radios> {
+        return this.prisma.radios.create({
+            data: createRadioAndPost(data.name, data.url_flux, data.url_img)
+        });
+    }
 
     async findAll(): Promise<Radios[] | null> {
         return this.prisma.radios.findMany();
